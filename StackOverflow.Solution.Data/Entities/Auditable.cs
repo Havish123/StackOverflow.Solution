c4807fd3644f60b4c6deabfd2a680d645ab898ca
Havishwaran Jeyakumar <havishwaran.jeyakumar@Impigertech.com>
2023-01-05 17:20:29 +0530

    Add User Table

---
+++ StackOverflow.Solution.Data/Entities/Auditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow.Solution.Data.Entities
+{
+    public class Auditable
+    {
+        public long Id { get; set; }
+        public DateTime CreatedDate{get;set;}
+        public long CreatedBy { get; set; }
+    }
+}
---
+++ StackOverflow.Solution.Data/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow.Solution.Data.Entities
+{
+    public class User: Auditable
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
